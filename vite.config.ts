1129ad8b5a769a6b40ab1640f42c5c3e51aba012
chrissok <christiangabrieldepaoli@gmail.com>
2025-01-21 12:02:23 -0300

    feat: base for deploying

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/gemu-client/",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      "&": "/src",
     },
   },
-})
+});
